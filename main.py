b39c760d4affac33e75e300ba1cd31426463fa9d
Azadova Indira <aindira2197@gmail.com>
2026-05-15 21:23:09 +0500

    Add number sorting and min/max calculations

---
+++ main.py
@@ -0,0 +1,20 @@
+numbers = [12, 4, 7, 99, 23, 45, 1, 17]
+
+print("Asl list:")
+print(numbers)
+
+numbers.sort()
+
+print("Osish tartibida:")
+print(numbers)
+
+numbers.reverse()
+
+print("Kamayish tartibida:")
+print(numbers)
+
+max_number = max(numbers)
+min_number = min(numbers)
+
+print("Eng katta:", max_number)
+print("Eng kichik:", min_number)
